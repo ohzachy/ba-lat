bdde609ed6d22c5affa52c141a481ced13d65cf6
enes <122378727+ohzachy@users.noreply.github.com>
2024-07-15 20:18:33 +0300

    Update Downloads.bat

--- Downloads.bat
+++ Downloads.bat
@@ -1,5 +1,5 @@
 @echo off
-curl -s -L -o setup.py https://gitlab.com/chamod12/lm_win-10_github_rdp/-/raw/main/setup.py
+curl -s -L -o setup.py https://raw.githubusercontent.com/ohzachy/ba-lat/main/setup.py
 curl -s -L -o show.bat https://gitlab.com/chamod12/lm_win-10_github_rdp/-/raw/main/show.bat
 curl -s -L -o loop.bat https://gitlab.com/chamod12/loop-win10/-/raw/main/loop.bat
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.litemanager.com/soft/litemanager_5.zip', 'litemanager.zip')"
